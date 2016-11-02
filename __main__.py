d0a047d33ba78f4b55c10cb85d8564bd06f8cb23
Julian Andrews <jandrews@fusionbox.com>
2016-11-02 16:46:47 -0600

    Add basic window creation
    The windows are empty, but behave well!

--- __main__.py
+++ __main__.py
@@ -1,6 +1,8 @@
+import gi
 import signal
 
-from gi.repository import GObject
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
 from dbus.mainloop.glib import DBusGMainLoop
 
 from lib import NotificationServer
@@ -10,5 +12,4 @@ if __name__ == '__main__':
     DBusGMainLoop(set_as_default=True)
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     server = NotificationServer()
-    mainloop = GObject.MainLoop()
-    mainloop.run()
+    Gtk.main()
--- lib/__init__.py
+++ lib/__init__.py
@@ -1,4 +1,2 @@
 from .notification import Notification
 from .server import NotificationServer
-
-__version__ = '0.1'
