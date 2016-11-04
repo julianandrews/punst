1214c4f187a8500b8ee44c796da70b6b37783ba7
Julian Andrews <jandrews@fusionbox.com>
2016-11-04 16:11:54 -0600

    Refactor overlong draw method

--- lib/gui.py
+++ lib/gui.py
@@ -10,13 +10,45 @@ from .utils import hex_to_rgb, format_text, NotificationUrgency
 class NotificationDrawingArea(Gtk.DrawingArea):
     def __init__(self, *args, **kwargs):
         super(NotificationDrawingArea, self).__init__(*args, **kwargs)
-        self.text = ""
         self.connect('draw', self.draw)
+        self.text = ""
         self.urgency = NotificationUrgency.NORMAL
 
+    def build_layout(self, cr):
+        layout = PangoCairo.create_layout(cr)
+        desc = Pango.FontDescription(settings.FONT)
+        layout.set_font_description(desc)
+        layout.set_width(Pango.SCALE * (
+            settings.WIDTH -
+            2 * (settings.PADDING[0] + settings.FRAME_WIDTH)
+        ))
+        layout.set_alignment(Pango.Alignment.LEFT)
+        layout.set_wrap(Pango.WrapMode.WORD_CHAR)
+        if settings.USE_MARKUP:
+            layout.set_markup(self.text, -1)
+        else:
+            layout.set_text(self.text, -1)
+        return layout
+
+    def position_window(self, width, height):
+        screen = Gdk.Screen.get_default()
+        monitor_rect = screen.get_monitor_geometry(settings.MONITOR_NUMBER)
+        x = monitor_rect.x + settings.X
+        if settings.X < 0:
+            x += monitor_rect.width - width
+        y = monitor_rect.y + settings.Y
+        if settings.Y < 0:
+            y += monitor_rect.height - height
+
+        parent = self.get_parent_window()
+        parent.resize(width, height)
+        parent.move(x, y)
+
     def draw(self, widget, cr):
-        width = cr.get_target().get_width()
-        height = cr.get_target().get_height()
+        layout = self.build_layout(cr)
+        width = settings.WIDTH
+        height = layout.get_pixel_size()[1] + 2 * (settings.PADDING[1] + settings.FRAME_WIDTH)
+
         bg_color = hex_to_rgb(settings.BG_COLORS[self.urgency])
         fg_color = hex_to_rgb(settings.FG_COLORS[self.urgency])
         if settings.FRAME_COLOR is None:
@@ -32,39 +64,12 @@ class NotificationDrawingArea(Gtk.DrawingArea):
         cr.set_line_width(2 * settings.FRAME_WIDTH)
         cr.stroke()
         cr.set_source_rgb(*fg_color)
-
-        layout = PangoCairo.create_layout(cr)
-        desc = Pango.FontDescription(settings.FONT)
-        layout.set_font_description(desc)
-        layout.set_width(Pango.SCALE * (
-            settings.WIDTH -
-            2 * (settings.PADDING[0] + settings.FRAME_WIDTH)
-        ))
-        layout.set_alignment(Pango.Alignment.LEFT)
-        layout.set_wrap(Pango.WrapMode.WORD_CHAR)
-        if settings.USE_MARKUP:
-            layout.set_markup(self.text, -1)
-        else:
-            layout.set_text(self.text, -1)
-        layout_height = layout.get_pixel_size()[1]
-        height = layout_height + 2 * (settings.PADDING[1] + settings.FRAME_WIDTH)
-        parent = self.get_parent_window()
-        parent.resize(settings.WIDTH, height)
-
-        screen = Gdk.Screen.get_default()
-        monitor_rect = screen.get_monitor_geometry(settings.MONITOR_NUMBER)
-        x = monitor_rect.x + settings.X
-        if settings.X < 0:
-            x += monitor_rect.width - settings.WIDTH
-        y = monitor_rect.y + settings.Y
-        if settings.Y < 0:
-            y += monitor_rect.height - height
-        parent.move(x, y)
-
         cr.translate(*settings.PADDING)
         cr.translate(settings.FRAME_WIDTH, settings.FRAME_WIDTH)
         PangoCairo.update_layout(cr, layout)
         PangoCairo.show_layout(cr, layout)
+
+        self.position_window(width, height)
 
 
 class NotificationWindow(Gtk.Window):
@@ -82,13 +87,10 @@ class NotificationWindow(Gtk.Window):
         self.set_size_request(settings.WIDTH, -1)
         self.connect('button-release-event', self.on_click)
 
-    def draw(self, summary, body, urgency):
+    def popup(self, summary, body, urgency):
         self.drawing_area.text = format_text(settings.FORMAT, summary, body)
         self.drawing_area.urgency = urgency
-        self.drawing_area.queue_draw()
-
-    def popup(self, summary, body, urgency):
-        self.draw(summary, body, urgency)
+        self.queue_draw()
         self.show()
         if self.timeout:
             GObject.source_remove(self.timeout)
